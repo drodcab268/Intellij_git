f215813dae0ff3d8dffc703a43fab5cdedcac53f
drodcab268 <danielroca26@gmail.com>
2023-12-18 10:46:10 +0100

    Se han corregido los errores

---
+++ src/Info_Producto/Producto.java
@@ -0,0 +1,32 @@
+package Info_Producto;
+
+public class Producto {
+
+    private String codigo;
+    private String nombre;
+    private float precio;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+}
